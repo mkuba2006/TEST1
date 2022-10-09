0855a174a806997b861e3679d8fce2ca65e4abe9
mkuba2006 <mkuba2006@gmail.com>
2022-10-09 20:42:03 +0200

    litle of work

---
+++ SCRIPTS/Main.js
@@ -0,0 +1,13 @@
+/*
+const nav = document.querySelector('nav');
+const nav_el = nav.querySelectorAll('a');
+*/
+
+
+$element = $('a#text')
+$element.on('click', function(){
+  $(this).addClass('active');
+})
+
+
+
