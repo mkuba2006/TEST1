186cec891b2c94589f8504814909a7f4cfb89874
mkuba2006 <mkuba2006@gmail.com>
2022-10-15 01:08:41 +0200

    1 1

--- SCRIPTS/Main.js
+++ SCRIPTS/Main.js
@@ -1,10 +1,29 @@
 /*
 const nav = document.querySelector('nav');
 const nav_el = nav.querySelectorAll('a');
+
+const contact = document.getElementById('content');
+const container = document.getElementById('container');
+const nav_a = document.querySelectorAll('nav a');
+const nav_a_active = document.querySelectorAll('nav a.active');
+
+
+nav_a.on().on('click', function(e){  
+
+  e.preventDefault();
+
+  var url = this.href;
+
+  nav_a_active.removeClass('active')
+  $(this).addClass('active')
+
+  container.remove();
+  $('#content').fadeOut('50000').hide();
+  $('#content').load(url + '#content').hide().fadeIn('500');
+
+})
+
 */
-
-
-
 
 $('nav a').on('click', function(e){
 
@@ -16,7 +35,7 @@ $('nav a').on('click', function(e){
   $(this).addClass('active')
   
   $('#container').remove();
-  $('#content').load(url + '#content').hide().fadeIn('200');
-  
-  
+  $('#content').fadeOut('50000').show();
+  $('#content').load(url + '#content').hide().fadeIn('500');
   })
+
