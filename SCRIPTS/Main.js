276df8aafef1e7b69601dd2356c0e3467f7fc893
mkuba2006 <mkuba2006@gmail.com>
2022-11-10 23:38:38 +0100

    next

--- SCRIPTS/Main.js
+++ SCRIPTS/Main.js
@@ -1,29 +1,3 @@
-/*
-const nav = document.querySelector('nav');
-const nav_el = nav.querySelectorAll('a');
-
-const contact = document.getElementById('content');
-const container = document.getElementById('container');
-const nav_a = document.querySelectorAll('nav a');
-const nav_a_active = document.querySelectorAll('nav a.active');
-
-
-nav_a.on().on('click', function(e){  
-
-  e.preventDefault();
-
-  var url = this.href;
-
-  nav_a_active.removeClass('active')
-  $(this).addClass('active')
-
-  container.remove();
-  $('#content').fadeOut('50000').hide();
-  $('#content').load(url + '#content').hide().fadeIn('500');
-
-})
-
-*/
 
 $('nav a').on('click', function(e){
 
@@ -33,7 +7,7 @@ $('nav a').on('click', function(e){
   
   $('nav a.active').toggleClass( "active" )
   $(this).toggleClass( "active" )
-
+  setTimeout(`location.href = '${url}';`,500);
   })
 
 
