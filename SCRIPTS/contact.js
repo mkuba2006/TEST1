d0a2495f91c4017a6ff9891fc5334def13ff4491
mkuba2006 <mkuba2006@gmail.com>
2022-11-19 18:20:44 +0100

    ...

---
+++ SCRIPTS/contact.js
@@ -0,0 +1,36 @@
+
+$('nav a').on('click', function(e){
+
+    e.preventDefault();
+    
+    var url = this.href;
+    
+    $('nav a.active').toggleClass( "active" )
+    $(this).toggleClass( "active" )
+    setTimeout(`location.href = '${url}';`,500);
+    })
+  $('#fa:last-child').hover(function () {
+    $(this).toggleClass( "fa-active" );
+    }
+  );
+  
+  $('#fa:last-child').hover(function () {
+    $(this).toggleClass( "fa-active" );
+    }
+  );
+
+  
+  const nav = document.querySelector('nav');
+  const nav_links = document.querySelectorAll('nav a');
+  const burger = document.querySelector('#burger');
+
+  burger.addEventListener('click', () => {
+  burger.classList.toggle('toggle');
+  nav.classList.toggle('nav_active');
+
+  nav_links.forEach((link , index) => {
+    link.style.animation = `navLink 0.5s ease forwards ${index / 7}s`;
+
+  })
+  })
+  
