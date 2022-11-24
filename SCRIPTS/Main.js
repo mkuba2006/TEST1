021c32ba3acbfc53a7901c04f713652a3a54fe22
mkuba2006 <mkuba2006@gmail.com>
2022-11-24 22:01:30 +0100

    ..

--- SCRIPTS/Main.js
+++ SCRIPTS/Main.js
@@ -1,4 +1,3 @@
-
 $('nav a').on('click', function(e){
 
   e.preventDefault();
@@ -36,17 +35,34 @@ $('#fa:last-child').hover(function () {
 
 
 
-  const nav = document.querySelector('nav');
-  const nav_links = document.querySelectorAll('nav a');
-  const burger = document.querySelector('#burger');
+ const nav = document.querySelector('nav');
+ const nav_links = document.querySelectorAll('nav a');
+ const burger = document.querySelector('#burger');
+ 
+ burger.addEventListener('click', () => {
+     burger.classList.toggle('toggle');
+     nav.classList.toggle('nav_active');
+     nav_links.forEach((link, index) => {
+         link.style.animation = `navLink 0.5s ease forwards ${index / 7}s`;
+     })
+ })
 
-  burger.addEventListener('click', () => {
-  burger.classList.toggle('toggle');
-  nav.classList.toggle('nav_active');
 
-  nav_links.forEach((link , index) => {
-    link.style.animation = `navLink 0.5s ease forwards ${index / 7}s`;
 
+
+
+const text = document.querySelectorAll('#text');
+
+text.forEach(text => {
+  text.addEventListener('mouseover', ()=>{
+    text.classList.add('color_new');
   })
+}); 
+
+text.forEach(text => {
+  text.addEventListener('mouseout', ()=>{
+    text.classList.remove('color_new');
   })
-  
+}); 
+
+
